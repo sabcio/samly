3d2f39d7d94c3b181ad257bb3e697fd087ff6353
Sebastian Wojtczak <wojtczaksebastian@gmail.com>
2024-12-06 15:06:01 +0100

    Log error

--- lib/samly/sp_handler.ex
+++ lib/samly/sp_handler.ex
@@ -57,6 +57,7 @@ defmodule Samly.SPHandler do
       {:error, reason} ->
         case idp do
           %IdpData{debug_mode: true} ->
+            Logger.error("Samly consume signin failed with #{inspect(reason)}")
             conn
             |> put_resp_header("content-type", "text/html")
             |> send_resp(403, "<html><body><div><h1>access_denied</h1><p><b>Error:</b><br /><pre><code>#{inspect(reason)}</code></pre></p><p><b>Raw Response:</b><br /><pre><code>#{saml_response}</code></pre></p></div></body></html")
